eb7b57a87df8a1ae905bea2937bd96c2f648dc7e
Bryan Aguilar <bryan.aguilar6174@gmail.com>
2024-11-08 17:07:03 -0500

    ✨  feat: Implement security configuration

---
+++ book_api/src/main/java/com/baguilar/book_api/security/UserDetailsServiceImpl.java
@@ -0,0 +1,22 @@
+package com.baguilar.book_api.security;
+
+import com.baguilar.book_api.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository repository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return repository.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+}
