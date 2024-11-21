683dc114639f1bd3f4937d46131db55dc8148f71
Bryan Aguilar <bryan.aguilar6174@gmail.com>
2024-11-21 16:46:42 -0500

    🔨 refactor: Remove extra files for Spring Security

--- book_api/src/main/java/com/baguilar/book_api/auth/AuthController.java
+++ book_api/src/main/java/com/baguilar/book_api/auth/AuthController.java
@@ -1,10 +1,7 @@
 package com.baguilar.book_api.auth;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -12,8 +9,6 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 @Tag(name = "Authentication")
 public class AuthController {
-
-    private final AuthenticationService service;
 
     @GetMapping("/hello")
     public String helloPublic() {
@@ -23,12 +18,5 @@ public class AuthController {
     @GetMapping("/hello-secure")
     public String helloSecure() {
         return "Hello world secure";
-    }
-
-    @PostMapping("/register")
-    @ResponseStatus(HttpStatus.ACCEPTED)
-    public ResponseEntity<?> register(@RequestBody @Valid RegistrationRequest request) {
-        service.register(request);
-        return ResponseEntity.accepted().build();
     }
 }
