0d33c5408b0c0e83bf1cc93db85eebfb0cf725a9
Bryan Aguilar <bryan.aguilar6174@gmail.com>
2024-12-06 20:11:05 -0500

    ✨  feature: Complete JWT utils methods

--- book_api/src/main/java/com/baguilar/book_api/utils/JwtUtil.java
+++ book_api/src/main/java/com/baguilar/book_api/utils/JwtUtil.java
@@ -1,13 +1,18 @@
 package com.baguilar.book_api.utils;
 
 import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.Claim;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
+import java.util.Map;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
@@ -35,5 +40,27 @@ public class JwtUtil {
                 .withJWTId(UUID.randomUUID().toString())
                 .withNotBefore(new Date(System.currentTimeMillis()))
                 .sign(algorithm);
+    }
+
+    public DecodedJWT validateToken(String token) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(this.privateKey);
+            JWTVerifier verifier = JWT.require(algorithm).withIssuer(this.userGenerator).build();
+            return verifier.verify(token);
+        } catch (JWTVerificationException exception) {
+            throw new JWTVerificationException("Invalid token, not authorized");
+        }
+    }
+
+    public String extractUsername(DecodedJWT decodedJWT) {
+        return decodedJWT.getSubject();
+    }
+
+    public Claim getSpecificClaim(DecodedJWT decodedJWT, String claimName) {
+        return decodedJWT.getClaim(claimName);
+    }
+
+    public Map<String, Claim> getAllClaims(DecodedJWT decodedJWT) {
+        return decodedJWT.getClaims();
     }
 }
